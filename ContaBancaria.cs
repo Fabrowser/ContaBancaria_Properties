abfd826c0dc60f5aa4f3695ef9f221d5b5062938
Fabrowser <79614829+Fabrowser@users.noreply.github.com>
2022-07-18 20:18:23 -0300

    Add files via upload

---
+++ ContaBancaria.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projetobanco
+{
+    class ContaBancaria
+    {
+        public int Conta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public int Teste;
+
+        public ContaBancaria()
+        {
+
+        }
+
+        public ContaBancaria(int conta, string nome)
+        {
+            Conta = conta;
+            Nome = nome;
+        }
+        public ContaBancaria(int conta, string nome, double deposito_inicial) : this(conta, nome)
+        {
+            Deposito(deposito_inicial);
+        }
+
+        public void Deposito(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Saque(double saque)
+        {
+            Saldo -= saque + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return ($"Conta :{Conta} \nNome: {Nome} \nSaldo: R$ {Saldo.ToString("F2")} ");
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Projetobanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            ContaBancaria c1;
+            c1 = new ContaBancaria();
+
+            Console.WriteLine("Numero da conta: ");
+            int numero_conta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nome: ");
+            string nome = Console.ReadLine();
+
+
+            Console.WriteLine("Deposito Inicial? [S/N]");
+            string tem_dep_ini = Console.ReadLine();
+
+            if (tem_dep_ini == "s" || tem_dep_ini == "S")
+            {
+                Console.WriteLine("Digite o Deposito Inicial");
+                double dep_ini = double.Parse(Console.ReadLine());
+                c1 = new ContaBancaria(numero_conta, nome, dep_ini);
+            }
+            else
+            {
+                c1 = new ContaBancaria(numero_conta, nome);
+            }
+
+            Console.WriteLine(c1);
+
+            Console.WriteLine("Deposito: ");
+            double deposito = double.Parse(Console.ReadLine());
+            c1.Deposito(deposito);
+            Console.WriteLine(c1);
+
+
+            Console.WriteLine("Saque: ");
+            c1.Saque(double.Parse(Console.ReadLine()));
+            Console.WriteLine(c1);
+
+
+        }
+    }
+}
